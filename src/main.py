3ba32d2fef0552b8f886f921ccd7460a3e994008
dceoy <dceoy@users.noreply.github.com>
2025-01-25 00:59:50 +0900

    Replace requirements.txt with pyproject.toml

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""DynamoDB CRUD operations using AWS Lambda and API Gateway."""
 
 import json
 import os
@@ -27,14 +28,15 @@ dynamodb = boto3.client("dynamodb")
 
 @app.delete("/items/<item_id>")
 @tracer.capture_method
-def delete_item(item_id: str) -> Response:
+def delete_item(item_id: str) -> Response[str]:
     """Delete an item from the DynamoDB table by item ID.
 
     Args:
         item_id (str): The ID of the item to be deleted.
 
     Returns:
-        Response: A JSON response indicating whether the item was successfully deleted.
+        Response[str]: A JSON response with a success message.
+
     """
     logger.info("Deleting item with id: %s", item_id)
     dynamodb.delete_item(
@@ -44,23 +46,26 @@ def delete_item(item_id: str) -> Response:
         },
     )
     return Response(
-        status_code=HTTPStatus.OK.value,                # 200
-        content_type=content_types.APPLICATION_JSON,    # application/json
+        status_code=HTTPStatus.OK.value,  # 200
+        content_type=content_types.APPLICATION_JSON,  # application/json
         body=json.dumps({"message": f"Deleted item {item_id}"}),
     )
 
 
 @app.get("/items/<item_id>")
 @tracer.capture_method
-def get_item(item_id: str) -> Response:
+def get_item(item_id: str) -> Response[str]:
     """Retrieve a specific item from the DynamoDB table by item ID.
 
     Args:
         item_id (str): The ID of the item to be retrieved.
 
     Returns:
-        Response: A JSON response containing the item data if found,
-        otherwise a 404 Not Found response.
+        Response[str]: A JSON response containing the item details.
+
+    Raises:
+        NotFoundError: If the item is not found in the table
+
     """
     logger.info("Fetching item with id: %s", item_id)
     result = dynamodb.get_item(
@@ -79,7 +84,7 @@ def get_item(item_id: str) -> Response:
             "price": float(item["price"]["N"]),
         }
         return Response(
-            status_code=HTTPStatus.OK.value,              # 200
+            status_code=HTTPStatus.OK.value,  # 200
             content_type=content_types.APPLICATION_JSON,  # application/json
             body=json.dumps(response_body),
         )
@@ -87,11 +92,12 @@ def get_item(item_id: str) -> Response:
 
 @app.get("/items")
 @tracer.capture_method
-def get_all_items() -> Response:
+def get_all_items() -> Response[str]:
     """Retrieve all items from the DynamoDB table.
 
     Returns:
-        Response: A JSON response containing a list of all items in the table.
+        Response[str]: A JSON response containing all items in the table.
+
     """
     logger.info("Fetching all items")
     result = dynamodb.scan(TableName=os.environ["DYNAMODB_TABLE_NAME"])
@@ -105,22 +111,26 @@ def get_all_items() -> Response:
         for i in items
     ]
     return Response(
-        status_code=HTTPStatus.OK.value,                # 200
-        content_type=content_types.APPLICATION_JSON,    # application/json
+        status_code=HTTPStatus.OK.value,  # 200
+        content_type=content_types.APPLICATION_JSON,  # application/json
         body=json.dumps(response_body),
     )
 
 
 @app.put("/items")
 @tracer.capture_method
-def put_item() -> Response:
+def put_item() -> Response[str]:
     """Add or update an item in the DynamoDB table.
 
     The request body must contain 'id', 'name', and 'price' fields.
     If any of these fields are missing, a 400 Bad Request response is returned.
 
     Returns:
-        Response: A JSON response with a success message, or a 400 Bad Request.
+        Response[str]: A JSON response with a success message.
+
+    Raises:
+        BadRequestError: If any of the required fields are missing.
+
     """
     body = app.current_event.json_body
     if any((k not in body) for k in ("id", "name", "price")):
@@ -136,8 +146,8 @@ def put_item() -> Response:
             },
         )
         return Response(
-            status_code=HTTPStatus.OK.value,                # 200
-            content_type=content_types.APPLICATION_JSON,    # application/json
+            status_code=HTTPStatus.OK.value,  # 200
+            content_type=content_types.APPLICATION_JSON,  # application/json
             body=json.dumps({"message": f"Put item {body['id']}"}),
         )
 
@@ -158,6 +168,7 @@ def lambda_handler(event: dict[str, Any], context: LambdaContext) -> dict[str, A
 
     Returns:
         dict[str, Any]: A dictionary representing the API Gateway response.
+
     """
     logger.info("Event received")
     return app.resolve(event, context)
